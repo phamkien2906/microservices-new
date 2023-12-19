3bde040a5db422d14163db3eeab0b8a961752942
PhamKien <phamtrungkienonion@gmail.com>
2023-12-19 16:28:00 +0700

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.programming.techie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
