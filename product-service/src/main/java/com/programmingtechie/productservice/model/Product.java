b1367c40f4c0c1300091217ac74b6863d40b23a9
PhamKien <phamtrungkienonion@gmail.com>
2024-05-27 17:02:02 +0700

    update

--- product-service/src/main/java/com/programmingtechie/productservice/model/Product.java
+++ product-service/src/main/java/com/programmingtechie/productservice/model/Product.java
@@ -4,17 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import java.math.BigDecimal ;
-@Document(value = "product")
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
+@Table(name = "product")
 public class Product {
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String description;
     private BigDecimal price;
